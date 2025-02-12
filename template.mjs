d3d06aa86dd13e05dd4c40a45062df840739bec7
idaspa <idahelenhal@msn.com>
2025-02-12 16:53:53 +0100

    firsr

---
+++ template.mjs
@@ -0,0 +1,53 @@
+// Your program should start at this line.
+let car = car;
+let turnHead = turn;
+
+while (peek === true) {
+    move ++
+}
+    if(peek()=== false){
+        for(let i = 0; i < turn.length i)
+    
+}
+
+
+/*
+Example:
+move();
+move();
+move();
+turn();
+move();
+move();
+turn();
+turn();
+turn();
+move();
+*/
+
+
+//#region Basic functions
+// These functions are just her to make your intelisense work. 
+// They only have a conceptual function.
+
+function move() {
+    // Moves the car 1 cell in the direction it is heading. 
+}
+
+function turn() {
+    // Turns the car 90 deg clockwise.
+}
+function turnLeft (){
+//turns left 
+}
+function peek() {
+    // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+function atGoal() {
+    // Returns true if the current cell is the goal cell.
+    return true; // just a placholder
+}
+
+//#endregion
